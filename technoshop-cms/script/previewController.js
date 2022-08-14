d1b079075c6f7d31f051314af9ccea20b453d62f
keaasd <kea-asd@yandex.ru>
2022-08-15 00:22:54 +0700

    лв

--- technoshop-cms/script/previewController.js
+++ technoshop-cms/script/previewController.js
@@ -1,27 +1,32 @@
 import { form, preview } from "./elems.js";
 import { toBase64 } from "./utils.js";
 
-export const showPreview = (src) => {
-    preview.getElementsByClassName.display = 'block';
+const showPreview = (src) => {
+    // preview.getElementsByClassName.display = 'block';
+    preview.style.display = 'block';
     preview.src = src;
 }
 
 const hidePreview = () => {
-    preview.getElementsByClassName.display = '';
+    preview.style.display = '';
     preview.src = '';
 }
 
 export const previewController = () => {
     // form.image здесь image - это name='image' из инпута.
     const imageFile = form.image;
-    // изменения событие
+    // событие изменения 
+    console.log(imageFile)
     image.addEventListener('change', async () => {
         if (imageFile.files.length) {
             // toBase64(imageFile.files[0]);
+            console.log(toBase64(imageFile.files[0]))
             const src = await toBase64(imageFile.files[0]);
             showPreview(src)
         }
         // console.log(image.files)
     })
 };
-console.log(toBase64(image.files));
+
+// console.log(toBase64());
+// 1:02 трансляция 
